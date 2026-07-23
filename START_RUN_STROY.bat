23b1a7120b71c6227b695ad40009d64dcdfb2ca9
ZhivchikZer <ZhivchikZer@users.noreply.github.com>
2026-07-23 06:53:16 +0300

    Increase preview token size

--- START_RUN_STROY.bat
+++ START_RUN_STROY.bat
@@ -48,7 +48,7 @@ echo [Server] http://localhost:3000
 echo [Server] Stop: Ctrl+C or close this window
 echo.
 
-start http://localhost:3000/?v=4
+start http://localhost:3000/?v=5
 
 python --version >nul 2>&1
 if %errorlevel% equ 0 (
